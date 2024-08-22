ee2ab323f8f422c89d747ce9d7655e4593ea89d9
ikitenik <ikitenokai@gmail.com>
2024-08-22 12:49:38 +0700

    update test.py

---
+++ tests/test.py
@@ -0,0 +1,43 @@
+import requests
+
+"hi"
+def answer(response):
+    if response.status_code == 200:
+        print('Success:', response.json())
+    else:
+        print('Failed:', response.status_code, response.text)
+
+
+def get(url_get, pk=None):
+    if pk:
+        response_get = requests.get(url_get + f'{pk}/')
+        answer(response_get)
+        return
+    response_get = requests.get(url_get)
+    answer(response_get)
+
+
+def put(url_put, pk, data_put):
+    response_put = requests.put(url_put + f'{pk}/', json=data_put)
+    answer(response_put)
+
+
+def post(url_post, data_put):
+    response_post = requests.post(url_post, json=data_put)
+    answer(response_post)
+
+
+def delete(url_delete, pk=None):
+    response_delete = requests.delete(url_delete + f'{pk}/')
+    answer(response_delete)
+
+
+url = 'http://localhost:8000/api/materials/'  # Полный адрес эндпоинта
+
+data = {
+    'type': 1,
+    'material_name': 'test1000',
+    'material_price': 1000
+}
+
+get(url, 37)
